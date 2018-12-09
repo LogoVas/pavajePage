1989cc4c2182f379b2689d5125ace92de4afccc8
gfx96 <33156276+gfx96@users.noreply.github.com>
2018-12-09 21:45:41 +0200

    Index,modele
    Added index  routedesign improvements.
    Added modele route new modal
    responsive images.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine","ejs");
 
 
 app.get("/",(req,res)=>{
-    res.render("index"); //
+    res.render("index"); 
 });
 
 app.get("/contact",(req, res) =>{
--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,15 @@
 // on page load script
 $(document).ready(function () {
-
+//   carousel settings
+    $('#carousel').carousel({
+        interval: 3000
+    })
 
 });
 
 function showModalAndcarousel(x){
-    // carousel settings
-    $('#carousel').carousel({
-        interval: 3000
-    })
-    $('#carousel').modal('show');
+
+    $('#modal').modal('show');
     // x = what image will be displayed first on modal
     $('#carouselExampleIndicators').carousel(x);
 }
